979ae03a1c5994a633dac0d608ad459c3083f630
Surya-Sriramoju <saisurya97@gmail.com>
2023-12-20 01:30:41 -0500

    T12

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 /**
  * @file main.cpp
  * @author Tarun Trilokesh
+ * @author Sai Surya Sriramoju
  * @date 12/12/2023
  * @version 1.0
  * 
@@ -22,35 +23,6 @@
  * navigation and inspection in a warehouse environment.
  * 
  * @return int - Returns 0 on successful execution.
- */
-// int main(int argc, char **argv) {
-//     rclcpp::init(argc, argv);
-//     InspectorBot inspector;
-
-//     inspector.setLoc(0.0, 2.0);
-//     inspector.goToLocation();
-//     inspector.setLoc(0.0, 4.0);
-//     inspector.goToLocation();
-//     // inspector.rotateBot();
-    
-    
-    // inspector.setLoc(1.5, 2.0);
-    // inspector.goToLocation();
-    // inspector.rotateBot();
-    // inspector.setLoc(3.0, 0.0);
-    // inspector.goToLocation();
-    // inspector.rotateBot();
-    // inspector.setLoc(4.0, 0.0);
-    // inspector.goToLocation();
-    // inspector.rotateBot();
-    // inspector.setLoc(0.5, 4.0);
-    // inspector.goToLocation();
-    // inspector.rotateBot();
-
-//     inspector.continueInspection();
-
-//     rclcpp::shutdown();
-// }
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
