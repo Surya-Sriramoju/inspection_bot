fd5d6c518bf4a9a371f83daccfc460b780edaa86
tarunreddyy <tarunsaireddy1998@gmail.com>
2023-12-20 12:21:22 -0500

    Test file updated

--- build/CMakeFiles/test_inspector_bot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_inspector_bot.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tarun/ros2_final/src/inspection_bot
-# Build directory: /home/tarun/ros2_final/src/inspection_bot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/tarun/ros2_final/src/inspection_bot/build/test_inspector_bot[1]_include.cmake")
-subdirs("_deps/googletest-build")
