d865293847dfbf73f918b1131c62492e95d9a8c8
tarunreddyy <tarunsaireddy1998@gmail.com>
2023-12-20 13:48:47 -0500

    Test file updated

--- test/InspectorBotTest.cpp
+++ test/InspectorBotTest.cpp
@@ -17,38 +17,65 @@
 #include "InspectorBot.hpp"
 
 // Mock version of InspectorBot for testing
-class MockInspectorBot {
+class MockInspectorBot : public InspectorBot {
 public:
-    MockInspectorBot() {
+    MockInspectorBot() : InspectorBot() {
         // Mock initialization, no ROS dependencies
     }
-    // Add any necessary mock methods here
+
+    // Mock methods to simulate behavior
+    void goToLocation() override {
+        // Simulate robot moving to a location
+    }
+
+    void rotateBot() override {
+        // Simulate robot rotation
+    }
+
+    void continueInspection() override {
+        // Simulate continuing inspection
+    }
 };
 
 class InspectorBotTest : public ::testing::Test {
 protected:
-    MockInspectorBot* bot;
+    std::unique_ptr<MockInspectorBot> bot;
 
     void SetUp() override {
-        bot = new MockInspectorBot();
+        bot = std::make_unique<MockInspectorBot>();
     }
 
     void TearDown() override {
-        delete bot;
+        bot.reset();
     }
 };
 
-TEST_F(InspectorBotTest, DummyTest1) {
-    EXPECT_TRUE(true); // Always true, test will always pass
+TEST_F(InspectorBotTest, TestSetAndGetLocation) {
+    bot->setLoc(5.0, 10.0);
+    EXPECT_EQ(bot->getLocx(), 5.0);
+    EXPECT_EQ(bot->getLocy(), 10.0);
 }
 
-TEST_F(InspectorBotTest, DummyTest2) {
-    EXPECT_EQ(1, 1); // Always equal, test will always pass
+TEST_F(InspectorBotTest, TestGoToLocation) {
+    // Test the goToLocation method
+    bot->setLoc(3.0, 4.0);
+    bot->goToLocation();
+    // Add assertions to validate the behavior
 }
 
-TEST_F(InspectorBotTest, DummyTest3) {
-    EXPECT_NE(1, 0); // Always not equal, test will always pass
+TEST_F(InspectorBotTest, TestRotateBot) {
+    // Test the rotateBot method
+    bot->rotateBot();
+    // Add assertions to validate the behavior
 }
+
+TEST_F(InspectorBotTest, TestContinueInspection) {
+    // Test the continueInspection method
+    bot->continueInspection();
+    // Add assertions to validate the behavior
+}
+
+// Add more tests as needed...
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
